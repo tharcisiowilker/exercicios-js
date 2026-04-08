3bc70e201e8f333b9ab15aaa07c76d33ca4be719
tharcisiowilker <daso@aluno.ifnmg.edu.br>
2026-04-08 23:24:40 +0000

    resoluçao aula 5

--- exercicios/aula05/index.js
+++ exercicios/aula05/index.js
@@ -20,20 +20,42 @@ export function classificarIdade(idade) {
 
 // Exercício 2 — calcularDesconto
 export function calcularDesconto(preco, percentual) {
-  // escreva seu código aqui
+  return preco - (preco * (percentual / 100));
 }
 
 // Exercício 3 — verificarAprovacao
 export function verificarAprovacao(nota) {
-  // escreva seu código aqui
+  if (nota >= 60) {
+    return 'aprovado';
+  }
+  else if (nota >= 40) {
+    return 'recuperação'
+  }
+  else {
+    return 'reprovado';
+  }
 }
 
 // Exercício 4 — somarPares
 export function somarPares(numeros) {
-  // escreva seu código aqui
+  let soma = 0;
+
+  for(let numero of numeros) {
+    if (numero % 2 === 0) {
+      soma += numero;
+    }
+  }
+  return soma;
 }
 
 // Exercício 5 — contarLetra
 export function contarLetra(texto, letra) {
-  // escreva seu código aqui
+  let contado = 0;
+
+  for(let caractere of texto) {
+    if (caractere === letra) {
+      contado = contado + 1;
+    }
+  }
+  return contado;
 }
