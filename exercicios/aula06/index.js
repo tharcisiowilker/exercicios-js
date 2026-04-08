9cf9401670dcf992c4f2acc9a82e73831fe9e286
tharcisiowilker <daso@aluno.ifnmg.edu.br>
2026-04-08 23:56:18 +0000

    feat: resoluçao aula 6

--- exercicios/aula06/index.js
+++ exercicios/aula06/index.js
@@ -7,25 +7,30 @@
 
 // Exercício 1 — criarSaudacao
 export function criarSaudacao(nome) {
-  // escreva seu código aqui
+  return `Olá, ${nome}!`;
 }
 
 // Exercício 2 — filtrarAprovados
 export function filtrarAprovados(alunos) {
-  // escreva seu código aqui
+  return alunos.filter(aluno => aluno.nota >= 60);
 }
 
 // Exercício 3 — extrairNomes
 export function extrairNomes(alunos) {
-  // escreva seu código aqui
+  return alunos.map(aluno => aluno.nome);
 }
 
 // Exercício 4 — buscarAluno
 export function buscarAluno(alunos, nome) {
-  // escreva seu código aqui
+  return alunos.find(aluno => aluno.nome === nome);
 }
 
 // Exercício 5 — calcularMedia
 export function calcularMedia(notas) {
-  // escreva seu código aqui
+  if (notas.length === 0) {
+    return 0;
+  }
+  const soma = notas.reduce((acumulador, nota) => acumulador + nota, 0);
+  
+  return soma / notas.length;
 }
